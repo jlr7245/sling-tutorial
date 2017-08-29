1fcc1c1998a9eb891e04024efc593fa10597326f
J Silverstein <jess.pebble@gmail.com>
2017-08-29 14:58:35 -0400

    react/redux boilerplate

---
+++ web/src/containers/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from '../components/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="app">
+          <Route exact path="/" component={Home} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
